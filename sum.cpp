0ccd6f177d44bc9d708ad17c11ec43e3e8b28de1
sherwinmoses01 <sherwinmoses01@gmail.com>
2026-07-15 15:20:34 +0530

    sum.cpp created

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+    cout << "Enter a number : ";
+    cin >> num1;
+    cout << "Enter another number : ";
+    cin >> num2;
+    cout << "The sum is : " << num1 + num2 << endl;
+    return 0;
+}
