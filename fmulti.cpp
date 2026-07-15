2196188aed0e8154e770693a0bb9fc64c3d09264
sherwinmoses01 <sherwinmoses01@gmail.com>
2026-07-15 15:37:18 +0530

    fmulti.cpp created

---
+++ fmulti.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float num1;
+    float num2;
+
+    cout << "Enter a number : ";
+    cin >> num1;
+    cout << "Enter another number : ";
+    cin >> num2;
+    
+    cout << "The product is : " << num1 * num2 << "\n";
+    
+    return 0;
+}
