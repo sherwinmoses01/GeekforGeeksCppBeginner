c6432e41f2825361812f3e22e653864889c494f5
sherwinmoses01 <sherwinmoses01@gmail.com>
2026-07-17 18:43:39 +0530

    diamond.cpp created

---
+++ diamond.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void pyramid(int row);
+
+int main(){
+    pyramid(5);
+}
+
+void pyramid(int row){
+
+    for (int i = 1;i<= row;i++){
+
+        int last = i * 2 - 1;
+
+        for (int j = 1;j <= row - i;j++){
+            cout << " ";
+        }
+        for (int k = 1;k<=last;k++){
+            cout << "*";
+        }
+        cout << "\n";
+
+    }
+
+    for (int i = row - 1;i>= 1;i--){
+
+        int last = i * 2 - 1;
+
+        for (int j = 1;j <= row - i;j++){
+            cout << " ";
+        }
+        for (int k = 1;k<=last;k++){
+            cout << "*";
+        }
+        cout << "\n";
+
+    }
+}
